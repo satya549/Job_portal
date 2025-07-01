2f00546e96d92b207a975965c718fe52104d9828
Satyadev Maurya <in.satyadev@gmail.com>
2025-07-01 23:24:20 +0530

    add HeroSection

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,10 @@
+
+const HeroSection = () => {
+  return (
+    <div className='text-center'>
+      <span className="px-4 py-2 rounded-full bg-gray-100 text-[#f83002] font-medium">No 1 Job Hunt WebSite</span>
+    </div>
+  )
+}
+
+export default HeroSection
