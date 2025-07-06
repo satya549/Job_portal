7f4574b50757fcf20d055e845016baf3b00093da
Satyadev Maurya <in.satyadev@gmail.com>
2025-07-06 23:05:24 +0530

    Update heroSection

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,10 +1,18 @@
-
 const HeroSection = () => {
   return (
-    <div className='text-center'>
-      <span className="px-4 py-2 rounded-full bg-gray-100 text-[#f83002] font-medium">No 1 Job Hunt WebSite</span>
+    <div className="text-center">
+      <div className="flex flex-col gap-5 my-10">
+        <span className="mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#f83002] font-medium">
+          No 1 Job Hunt WebSite
+        </span>
+        <h1 className="text-5xl font-bold">
+          Search, Apply & <br /> Get Your{" "}
+          <span className="text-[#6A38C2]">Dream Jobs</span>
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi unde non nostrum est dicta.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
