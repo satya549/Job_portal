b208a4c5b1f7286a7a114a9530e913ea93a428fd
Satyadev Maurya <in.satyadev@gmail.com>
2025-08-04 22:41:20 +0530

    LatestJobCards done

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -1,13 +1,20 @@
+import { Badge } from "./ui/badge";
+
 const LatestJobCards = () => {
   return (
-    <div>
+    <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer">
       <div>
-        <h3>Company name</h3>
-        <p>India</p>
+        <h2 className="font-medium text-lg">Company name</h2>
+        <p className="text-sm text-gray-500">India</p>
       </div>
       <div>
-        <h3>Job Title</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        <h2 className="font-bold text-lg my-2">Job Title</h2>
+        <p className="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+      </div>
+      <div className="flex items-center gap-2 mt-4">
+        <Badge className={"text-blue-700 font-bold"} variant= "ghost">12 Position</Badge>
+        <Badge className={"text-[#f83002] font-bold"} variant= "ghost">Part Time</Badge>
+        <Badge className={"text-[#7209b7] font-bold"} variant= "ghost">12 LPA</Badge>
       </div>
     </div>
   );
